84fe5b9db1e51a4978f27765f8e85db9b705d8ec
Pedro <57113368+Volac8@users.noreply.github.com>
2026-01-19 12:16:55 -0300

    Delete jarvis.py

--- jarvis.py
+++
@@ -1,8 +0,0 @@
-# no topo do seu jarvis.py, junto com os outros imports:
-#Aqui ficaram os comandos personalizados / evoluções do jarvis
-import importlib, comandos_personalizados
-
-# Carrega quaisquer comandos salvos
-importlib.reload(comandos_personalizados)
-
-# ID do cargo que pode criar comandos (substitua pelo real)
